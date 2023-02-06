71b8464d08faabc3657118d94255ecde2f9034a2
Igor <igor8244@gmail.com>
2023-02-06 18:41:39 -0300

    Classe position base

---
+++ src/application/Main.java
@@ -0,0 +1,12 @@
+package application;
+
+import boardgame.Position;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Position p = new Position(1,2);
+		System.out.println(p);
+	}
+
+}
