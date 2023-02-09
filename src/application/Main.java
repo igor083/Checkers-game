2cf92b0e6831ce529278c3ae6790f0ea9060d178
Igor <igor8244@gmail.com>
2023-02-09 14:06:28 -0300

    Tratamento de excecao

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,7 +1,9 @@
 package application;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import checkers.CheckersException;
 import checkers.CheckersMatch;
 import checkers.CheckersPiece;
 import checkers.CheckersPosition;
@@ -12,17 +14,30 @@ public class Main {
 		CheckersMatch match = new CheckersMatch();
 		Scanner sc = new Scanner(System.in);
 		while (true) {
-			UserInterface.printBoard(match.getPieces());
-			System.out.println();
-			System.out.print("Source: ");
-			CheckersPosition source = UserInterface.readCheckersPosition(sc);
-			System.out.println();
-			System.out.print("Target: ");
-			CheckersPosition target = UserInterface.readCheckersPosition(sc);
-			
-			CheckersPiece capturedPiece = match.performCheckersMove(source, target);
-			
-			
+			try {
+				UserInterface.clearScreen();
+				UserInterface.printBoard(match.getPieces());
+				System.out.println();
+				System.out.print("Source: ");
+				CheckersPosition source = UserInterface.readCheckersPosition(sc);
+				System.out.println();
+				System.out.print("Target: ");
+				CheckersPosition target = UserInterface.readCheckersPosition(sc);
+
+				CheckersPiece capturedPiece = match.performCheckersMove(source, target);
+			}catch (CheckersException e) {
+				
+				System.out.println(e.getMessage());
+				sc.nextLine();
+				
+			} catch (InputMismatchException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
+					
+				
+				
+
 		}
 	}
 
